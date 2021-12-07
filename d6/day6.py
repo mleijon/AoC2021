f2204f71751820da20cd0fed2e1f99e36f828340
mleijon <mikael.leijon@gmail.com>
2021-12-07 10:20:21 +0100

    Done!

--- d6/day6.py
+++ d6/day6.py
@@ -1,20 +1,31 @@
+from collections import defaultdict
+
+
 def next_gen(old_gen, day):
-    new_gen = []
-    for fish in old_gen:
-        if fish in [1, 2, 3, 4, 5, 6, 7, 8]:
-            new_gen.append(fish - 1)
-        else:
-            new_gen.extend([0, 8])
-    if day == 80:
-        return len(new_gen)
+    new_gen = defaultdict(lambda: 0)
+    new_gen[1] = old_gen[2]
+    new_gen[2] = old_gen[3]
+    new_gen[3] = old_gen[4]
+    new_gen[4] = old_gen[5]
+    new_gen[5] = old_gen[6]
+    new_gen[6] = old_gen[7] + old_gen[0]
+    new_gen[7] = old_gen[8]
+    new_gen[8] = old_gen[0]
+    new_gen[0] = old_gen[1]
+    if day == 256:
+        return sum(new_gen.values())
     else:
-        print('day: {}'.format(day))
-        print('nr of fish: {}'.format(len(new_gen)))
         day += 1
         return next_gen(new_gen, day)
 
 
 if __name__ == '__main__':
+    current_gen = defaultdict(lambda: 0)
     with open('d6/d6_inp.txt') as f:
         lanternfish = [int(x) for x in f.read().split(',')]
-    print(next_gen(lanternfish, 1))
+    for item in lanternfish:
+        current_gen[item] += 1
+    print(next_gen(current_gen, 1))
+
+
+
